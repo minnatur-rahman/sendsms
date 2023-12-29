3429fa666ec469b57f721f14309dbf68ec20016f
millatur <minnatur7373@gmail.com>
2023-12-29 21:53:54 +0600

    minnatur commit

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,10 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SMSController;
 use App\Http\Controllers\WebController;
+use App\Http\Middleware\AdminGard;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,7 +13,13 @@ Route::get('/', function () {
 
 
 
+Route::prefix('admin')->name('admin.')->middleware([AdminGard::class])->group(function(){
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/sms', [SMSController::class, 'index']);
-Route::get('/profile',[WebController::class, 'index']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/sms/send', [SMSController::class, 'sms_send'])->name('sms.send');
+    Route::get('/sms/list', [SMSController::class, 'index'])->name('sms.list');
+    Route::get('/profile',[WebController::class, 'index']);
+
+});
+
+
