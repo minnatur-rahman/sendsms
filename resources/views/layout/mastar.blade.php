60e0686c36503edb57bf979714b67b97594708af
millatur <minnatur7373@gmail.com>
2023-12-30 22:55:19 +0600

    mainnstur rah

--- resources/views/layout/mastar.blade.php
+++ resources/views/layout/mastar.blade.php
@@ -32,7 +32,7 @@
   </div>
   <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
   <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="../assets/js/sidebarmenu.js"></script>
+  <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
   <script src="../assets/js/app.min.js"></script>
   <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
   <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
