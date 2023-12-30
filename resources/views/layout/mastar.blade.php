40942fa6019096704afc8a3c335d908dce2b6757
millatur <minnatur7373@gmail.com>
2023-12-30 22:52:37 +0600

    minnatue rahmen

--- resources/views/layout/mastar.blade.php
+++ resources/views/layout/mastar.blade.php
@@ -30,8 +30,8 @@
       </div>
     </div>
   </div>
-  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
-  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
   <script src="../assets/js/sidebarmenu.js"></script>
   <script src="../assets/js/app.min.js"></script>
   <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
